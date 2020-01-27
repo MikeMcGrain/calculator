d4f04b712824eb5b760a4675438ea778a6315039
Mike McGrain <mcgrain.mike@gmail.com>
2020-01-27 11:38:19 -0600

    addEventListeners to buttons

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
-let numbers = document.getElementsByClassName(".numbers")
-console.log(numbers)
+// add listeners to number buttons
+numbers = document.getElementsByClassName("numbers")
+for (let number of numbers) {
+    number.addEventListener("click", function() {
+      console.log(number.innerText)
+    })
+}
+
+// add listeners to operation buttons 
+operations = document.getElementsByClassName("operations")
+for (let operation of operations) {
+    operation.addEventListener("click", function() {
+      console.log(operation.innerText)
+    })
+}
