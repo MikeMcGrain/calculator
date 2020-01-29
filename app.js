5329e052b30776f0e27bb4ce249f70e417863d1d
Mike McGrain <mcgrain.mike@gmail.com>
2020-01-29 14:07:41 -0600

    subgrids

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
 // add listeners to number buttons
-numbers = document.getElementsByClassName("numbers")
+let numbers = document.getElementsByClassName("numbers")
 for (let number of numbers) {
   number.addEventListener("click", function() {
-    displayScreen(number.innerText)
+    // displayScreen(number.innerText)
   })
 }
 
 // add listeners to operation buttons
-operations = document.getElementsByClassName("operations")
+let operations = document.getElementsByClassName("operations")
 for (let operation of operations) {
   operation.addEventListener("click", function() {
-    console.log(operation.innerText)
+    switch (operation.innerText) {
+      case "Clear": clearScreen()
+    }
   })
 }
 
 // render number clicked to display
 function displayScreen(arg) {
-  console.log(arg)
   let screen = document.getElementById("screen")
-  screen.value = arg
+  screen.value = screen.value + arg
+}
+
+// clear screen
+function clearScreen() {
+ displayScreen(0)
 }
